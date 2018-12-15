4ca893b8e4b50624eb141cb625d854e639aae6c8
vadymvoitko <vadym.voitko@gmail.com>
2018-12-15 10:12:04 +0200

    first code

--- routing_in_vue/src/main.js
+++ routing_in_vue/src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from 'vue-router'
 
 Vue.config.productionTip = false
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    {
+      name: 'Brands',
+      path: '/brands',
+      component: Brand
+    }
+  ]
+})
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
