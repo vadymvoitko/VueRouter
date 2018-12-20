365ee710da20c8f75386de2d7cc7bdb285399306
vadymvoitko <vadym.voitko@gmail.com>
2018-12-20 19:03:43 +0200

    .

--- routing_in_vue/src/App.vue
+++ routing_in_vue/src/App.vue
@@ -22,12 +22,38 @@ import Search from './components/Search'
 
 export default {
   name: 'app',
+  data () {
+    return {
+      reso: []
+    }
+  },
   components: {
     Search,
     Results
   },
+  methods: {
+    callMe() {
+      let p = new Promise((Resolve, Reject)=>{
+        setTimeout(() => {
+          Resolve([1,2,3])
+        }, 1500);
+      })
+      p.then(res => {
+        console.log('1 ', res)
+        this.reso.push(...res)
+        return new Promise((resolve) => {
+          resolve(res)
+        })
+      }).then(res => {
+        console.log(this.reso);
+        throw new Error('o_O');
+        })
+        .catch(err => console.log(err))
+    }
+  },
   created () {
     console.log(this.$store.getters)
+    this.callMe()
   }
 }
 </script>
