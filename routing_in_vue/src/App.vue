8f070a46bb091b664438db2ea8e75a9b994a2aad
vadymvoitko <vadym.voitko@gmail.com>
2018-12-20 19:06:35 +0200

    .

--- routing_in_vue/src/App.vue
+++ routing_in_vue/src/App.vue
@@ -1,59 +1,16 @@
 <template>
     <div id="app">
-    <header>
-      <div class="logo">
-        <img 
-        src="https://raw.githubusercontent.com/GitHubTochkaDev/vuejs-tutorials/vuex/src/assets/logo.png" 
-        alt=""
-        @click="$store.dispatch('loginLogout')"
-        >
-      </div>
-      <search></search>
-    </header>
-    <main>
-      <results></results>
-    </main>
+    
   </div>
 </template>
 
 <script>
-import Results from './components/Results'
-import Search from './components/Search'
-
 export default {
   name: 'app',
   data () {
     return {
-      reso: []
+      
     }
-  },
-  components: {
-    Search,
-    Results
-  },
-  methods: {
-    callMe() {
-      let p = new Promise((Resolve, Reject)=>{
-        setTimeout(() => {
-          Resolve([1,2,3])
-        }, 1500);
-      })
-      p.then(res => {
-        console.log('1 ', res)
-        this.reso.push(...res)
-        return new Promise((resolve) => {
-          resolve(res)
-        })
-      }).then(res => {
-        console.log(this.reso);
-        throw new Error('o_O');
-        })
-        .catch(err => console.log(err))
-    }
-  },
-  created () {
-    console.log(this.$store.getters)
-    this.callMe()
   }
 }
 </script>
--- routing_in_vue/src/main.js
+++ routing_in_vue/src/main.js
@@ -3,9 +3,6 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-import store from './store'
-
 new Vue({
-  render: h => h(App),
-  store
+  render: h => h(App)
 }).$mount('#app')
